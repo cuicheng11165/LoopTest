7dba979e3836de3d8bea7f58ee69e2b1d3ffa51f
simmon cui <cuicheng11165@outlook.com>
2024-03-31 16:35:05 +0800

    Initial commit

---
+++ LoopTest/Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.IdentityModel.Clients.ActiveDirectory;
+
+public class AzureDetails
+{
+    public static string ClientID = "<Client ID or App ID>";
+    public static string ClientSecret = "<Client Secret or Password>";
+    public static string TenantID = "<Tenant ID>";
+    public static string AccessToken { get; set; }
+
+
+    public static void GetAuthorizationToken()
+    {
+        ClientCredential cc = new ClientCredential(AzureDetails.ClientID, AzureDetails.ClientSecret);
+        var context = new AuthenticationContext("https://login.microsoftonline.com/" + AzureDetails.TenantID);
+        var result = context.AcquireTokenAsync("https://management.azure.com/", cc);
+        if (result == null)
+        {
+            throw new InvalidOperationException("Failed to obtain the Access token");
+        }
+
+        AzureDetails.AccessToken = result.Result.AccessToken;
+    }
+}
